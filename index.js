fa2a1543f1df6e837f0ceba4a269a26ef325f83c
ezolla <ezoller11@gmail.com>
2020-06-21 20:23:06 -0700

    Added configuration for successful Heroku usage

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 // Parse the request body
 app.use(bodyParser.json());
