1dd04fe793ded0554bf42bc2529f624fecda3a44
Alistair Smith <alistairsmith01@gmail.com>
2020-09-22 12:29:16 +0100

    Strongly type

--- src/index.ts
+++ src/index.ts
@@ -1,145 +1,98 @@
-// Importing packages
-require("dotenv").config();
-const express = require("express");
-const bodyParser = require("body-parser");
-const request = require("request-promise");
+import 'dotenv/config';
 
-// Initializing express app
+import express, { Request } from 'express';
+import { IncomingLinearWebhookPayload } from './types';
+import fetch from 'node-fetch';
+
 const app = express();
 
-// Setting port
-const port: string | number = process.env.PORT || 3000;
+const port: number = parseInt(process.env.PORT ?? '3000');
 
-// Parse the request body
-app.use(bodyParser.json());
+app.use(express.json());
 
-// Receive HTTP POST requests
-app.post("/linear", (req: any, res: any) => {
-  // TEMP: logging to learn types
-  console.log(`Request Type: ${typeof req}`);
-  console.log(`Response Type: ${typeof res}`);
-
+app.post<Request['params'], unknown, IncomingLinearWebhookPayload>('/linear', async (req, res) => {
   const payload = req.body;
 
-  // Checking for new issue event
-  if (payload.action == "create" && payload.type == "Issue") {
-    console.log("New Issue");
-    newIssue(payload);
+  if (payload.action === 'create' && payload.type === 'Issue') {
+    await newIssue(payload);
   }
 
-  // Responding with 200
   res.sendStatus(200);
 });
 
-app.listen(port, () =>
-  console.log(`Webhook consumer listening on port ${port}!`)
-);
+app.listen(port, () => console.log(`Webhook consumer listening on port ${port}!`));
 
-function newIssue(payload: any) {
-  // TEMP: logging to learn types
-  console.log(`Payload Type: ${typeof payload}`);
-
-  return new Promise((resolve, _reject) => {
-    request({
-      url: process.env.WEBHOOK,
-      method: "POST",
-      json: {
-        embeds: [
-          {
-            color: 0x4752b2,
-            author: {
-              name: `Issue Created [${getID(payload.url)}]`,
-            },
-            title: payload.data.title,
-            url: payload.url,
-            fields: [
-              {
-                name: "Priority",
-                value: getPriorityValue(payload.data.priority),
-                inline: true,
-              },
-              {
-                name: "Points",
-                value: payload.data.estimate,
-                inline: true,
-              },
-              {
-                name: "Labels",
-                value: prettifyLabels(payload.data.labels),
-                inline: false,
-              },
-            ],
-            timestamp: new Date(),
-            footer: {
-              text: `Linear App`,
-              icon_url:
-                "https://pbs.twimg.com/profile_images/1121592030449168385/MF6whgy1_400x400.png",
-            },
+function newIssue(payload: IncomingLinearWebhookPayload) {
+  return fetch(process.env.WEBHOOK!, {
+    method: 'POST',
+    body: JSON.stringify({
+      embeds: [
+        {
+          color: 0x4752b2,
+          author: {
+            name: `Issue Created [${getId(payload.url)}]`,
           },
-        ],
-      },
-    })
-      .then(() => {
-        console.log("Sent Hook");
-        resolve();
-      })
-      .catch(console.error);
+          title: payload.data.title,
+          url: payload.url,
+          fields: [
+            {
+              name: 'Priority',
+              value: getPriorityValue(payload.data.priority ?? 0),
+              inline: true,
+            },
+            {
+              name: 'Points',
+              value: payload.data.estimate,
+              inline: true,
+            },
+            {
+              name: 'Labels',
+              value: prettifyLabels(payload.data.labels!),
+              inline: false,
+            },
+          ],
+          timestamp: new Date(),
+          footer: {
+            text: `Linear App`,
+            icon_url: 'https://pbs.twimg.com/profile_images/1121592030449168385/MF6whgy1_400x400.png',
+          },
+        },
+      ],
+    }),
   });
 }
 
 /**
  * Get the Priority Value translated
- * @param {int} priority number for priority
+ * @param priority number for priority
  */
-function getPriorityValue(priority: any) {
-  // TEMP: logging to learn types
-  console.log(`Payload Type: ${typeof priority}`);
-
+function getPriorityValue(priority: NonNullable<IncomingLinearWebhookPayload['data']['priority']>) {
   switch (priority) {
     case 0:
-      return "None";
+      return 'None';
     case 1:
-      return "Urgent";
+      return 'Urgent';
     case 2:
-      return "High";
+      return 'High';
     case 3:
-      return "Medium";
+      return 'Medium';
     case 4:
-      return "Low";
+      return 'Low';
   }
 }
 
 /**
  * Get the task ID from url
- * @param {string} link task url
+ * @param link task url
  */
-function getID(link: any) {
-  // TEMP: logging to learn types
-  console.log(`Link Type: ${typeof link}`);
-
-  var id = link.split("/");
-
-  return id[5];
+function getId(link: string) {
+  return link.split('/')[5];
 }
 
 /**
  * Formats and prettifies label(s)
- * @param {Array} labels connected labels
+ * @param labels connected labels
  */
-function prettifyLabels(labels: any) {
-  // TEMP: logging to learn types
-  console.log(`Labels Type: ${typeof labels}`);
-
-  let payload = "";
-
-  labels.forEach((label: any) => {
-    // TEMP: logging to learn types
-    console.log(`Label Type: ${typeof label}`);
-
-    payload += `${label.name}, `;
-  });
-
-  payload = payload.substring(0, payload.length - 2);
-
-  return payload;
+function prettifyLabels(labels: NonNullable<IncomingLinearWebhookPayload['data']['labels']>) {
+  return labels.map((label) => label.name).join(', ');
 }
